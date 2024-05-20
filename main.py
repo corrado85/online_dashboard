f688e380511e67833a5689e187106386a2837c4a
corrado85 <77513385+corrado85@users.noreply.github.com>
2024-05-20 16:18:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,77 @@
-import datetime
+# Solo i tickers che ho in portafoglio
 
-def generate_monthly_report():
-    # Codice per generare il report mensile
-    report = f"Report del mese: {datetime.datetime.now().strftime('%B %Y')}\n"
-    report += "Dati e statistiche...\n"
-    return report
+prtf = ['1MC', '500', 'ANX', 'AAPL', 'CHIP', 'CFR', 'META', 'MSFT', 'MWRD', 'NVDA', 'STLA', 'TRMD']
 
-if __name__ == "__main__":
-    report = generate_monthly_report()
-    with open("report.txt", "w") as file:
-        file.write(report)
+symbol_list = prtf
+
+all_tickers_filtered = prtf
+
+all_tickers = prtf
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 6500 ticker ci mette un paio di minuti
+def fetch_fundamental_data(symbol):
+    try:
+        stock = yf.Ticker(symbol).info
+        return stock
+    except Exception as e:
+        print(f"Ignorato errore per il simbolo {symbol}: {e}")
+        return None
+
+# Specify the maximum number of threads (adjust according to your needs)
+max_threads = 10
+
+# Utilizza tqdm per aggiungere una progress bar al ciclo
+with concurrent.futures.ThreadPoolExecutor(max_threads) as executor:
+    # Use executor.map to parallelize the fetching of data
+    fundamental_data = list(tqdm(executor.map(fetch_fundamental_data, all_tickers), total=len(all_tickers), desc="Fetching data"))
+
+# Remove None values (symbols that had errors)
+fundamental_data = [data for data in fundamental_data if data is not None]
+
+# Visualizza il DataFrame finale
+df_fundamental = pd.DataFrame(fundamental_data)
+
+
+
+
+
+
+
+
+# Definisci la funzione che esegue la moltiplicazione
+def multiply_columns(df):
+    df['dividendYield'] *= 1
+    df['ebitdaMargins'] *= 1
+    df['returnOnEquity'] *= 1
+    df['priceToHighRatio'] = df['currentPrice'] / df['fiftyTwoWeekHigh'] * 100
+    return df.drop(columns=['currentPrice', 'fiftyTwoWeekHigh'])  #non mi interessa portarmi dietro df['currentPrice'] / df['fiftyTwoWeekHigh']
+
+
+# Filtra e seleziona il DataFrame, applica la funzione e ordina
+df_filtered = (
+    df_fundamental[
+        #(df_fundamental['industry'] == 'Oil & Gas Midstream') &
+    (df_fundamental['marketCap'] > 2000000000)]
+    [['shortName', 'marketCap', 'industry','dividendYield', 'ebitdaMargins', 'recommendationKey','totalCashPerShare',
+      'returnOnEquity', 'currentPrice', 'fiftyTwoWeekHigh'
+      #'priceToHighRatio'
+      ]]
+    .pipe(multiply_columns)
+    .sort_values(by='dividendYield', ascending=False)
+)
+
+#df_filtered = df_fundamental[(df_fundamental['industry'] == 'Oil & Gas Midstream') & (df_fundamental['marketCap'] > 2000000000)][['shortName', 'marketCap','52WeekChange','dividendYield','ebitdaMargins','recommendationKey']].assign(dividendYield=lambda x: x[['dividendYield','ebitdaMargins']] * 100).sort_values(by='dividendYield', ascending=False)
+df_filtered
+
